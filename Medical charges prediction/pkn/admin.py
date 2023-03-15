2a7659f665d35c4817b596f018bd40bcc2edc9b4
Preethi P <81736013+preethi-p86@users.noreply.github.com>
2023-03-15 12:01:35 +0530

    Delete Medical charges prediction directory

--- Medical charges prediction/pkn/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import register_new, login_new, contacting_new, predict,collect_new
-# Register your models here.
-
-admin.site.register(register_new)
-admin.site.register(login_new)
-admin.site.register(contacting_new)
-admin.site.register(predict)
-admin.site.register(collect_new)
